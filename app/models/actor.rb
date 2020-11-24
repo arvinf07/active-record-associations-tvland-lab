02444c2e951ae435a938360818759bcb9473b973
Arvin Fernandez <arvinf07@gmail.com>
2020-11-24 17:05:02 -0500

    commit tree c758d3451df8e5aa38a0ebfdff8e7c2cd4b51d60

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end  
+
+  def list_roles
+    characters = self.characters
+    shows = self.shows
+    characters.map.with_index {|character, i| "#{character.name} - #{shows[i].name}"}
+  end  
 end
